5ae2c90975bba449ba5ed0030603353ea12313ed
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-03-14 20:48:08 -0300

    feat: Adding pagination

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 SWAGGER_SETTINGS = {
